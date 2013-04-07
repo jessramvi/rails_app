2419132d6f27a8f278b430ef20b11c78455802ff
jess <jess@jess.(none)>
2013-04-07 17:09:20 +0100

    Primera app Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e1d7d9c6e98e91d2c1095eac17207c402caf79cec9c48f2dce29d0a8e198af8f81939995aedae6fe7412b961fa421f47017fe3402c559430fd81e274994c87dd'
